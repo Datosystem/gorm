a9565445ed2f5e5ac2d106446e7e23c730ca94c1
Davide Cesare <davide.cesare@datosystem.it>
2024-02-13 17:59:23 +0100

    Initial commit

---
+++ callbacks/callmethod.go
@@ -0,0 +1,26 @@
+package callbacks
+
+import (
+	"context"
+	"reflect"
+
+	"gorm.io/gorm"
+)
+
+func callMethod(db *gorm.DB, fc func(value interface{}, tx *gorm.DB) bool) {
+	tx := db.Session(&gorm.Session{NewDB: true})
+	// Custom
+	tx = tx.Statement.WithContext(context.WithValue(tx.Statement.Context, "parent", tx))
+	if called := fc(db.Statement.ReflectValue.Interface(), tx); !called {
+		switch db.Statement.ReflectValue.Kind() {
+		case reflect.Slice, reflect.Array:
+			db.Statement.CurDestIndex = 0
+			for i := 0; i < db.Statement.ReflectValue.Len(); i++ {
+				fc(reflect.Indirect(db.Statement.ReflectValue.Index(i)).Addr().Interface(), tx)
+				db.Statement.CurDestIndex++
+			}
+		case reflect.Struct:
+			fc(db.Statement.ReflectValue.Addr().Interface(), tx)
+		}
+	}
+}
---
+++ tests/go.mod
@@ -0,0 +1,18 @@
+module gorm.io/gorm/tests
+
+go 1.16
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/jinzhu/now v1.1.5
+	github.com/lib/pq v1.10.7
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	gorm.io/driver/mysql v1.4.6
+	gorm.io/driver/postgres v1.4.6
+	gorm.io/driver/sqlite v1.4.4
+	gorm.io/driver/sqlserver v1.4.2
+	gorm.io/gorm v1.24.5
+)
+
+replace gorm.io/gorm => ../
