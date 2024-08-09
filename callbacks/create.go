919398bfa630a7c66032e5eeca704199b4c1178e
Davide Cesare <davide.cesare@datosystem.it>
2024-08-09 17:07:42 +0200

    Possible fix for association with default value

--- callbacks/create.go
+++ callbacks/create.go
@@ -302,7 +302,7 @@ func ConvertToCreateValues(stmt *gorm.Statement) (values clause.Values) {
 				for _, column := range values.Columns {
 					if field := stmt.Schema.LookUpField(column.Name); field != nil {
 						if v, ok := selectColumns[field.DBName]; (ok && v) || (!ok && !restricted) {
-							if !field.PrimaryKey && (!field.HasDefaultValue || field.DefaultValueInterface != nil) && field.AutoCreateTime == 0 {
+							if !field.PrimaryKey && field.AutoCreateTime == 0 {
 								if field.AutoUpdateTime > 0 {
 									assignment := clause.Assignment{Column: clause.Column{Name: field.DBName}, Value: curTime}
 									switch field.AutoUpdateTime {
